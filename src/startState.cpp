b76bbb517cc9258fc114bef60da1044c3ab945f9
Akshay Srivatsan <akshay.srivatsan@gmail.com>
2016-10-01 21:47:41 -0400

    Updated highscore state to reflect new start state, and added music back to new start state

--- src/startState.cpp
+++ src/startState.cpp
@@ -15,6 +15,9 @@ StartState::~StartState() {
 }
 
 void StartState::begin() {
+    // play background music
+    this->soundHandler.playBackgroundMusic("music/mega_destruction_titlescreen.xm");
+
     Entity * mainText = entityBuilder.createFadeInText("resources/CaesarDressing-Regular.ttf", "CornerGame", 100,
     255, 255, 255, 0, this->windowW, this->windowH);
     entityMap.operator[](mainText->getId())= mainText;
@@ -56,4 +59,5 @@ void StartState::run() {
 }
 
 void StartState::cleanup() {
+    this->soundHandler.stopBackgroundMusic();
 }
