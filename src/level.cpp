ef1cde27fbefe94e0b09607ed7cf3e3333afed4d
Grady Xiao <xiao.grady@gmail.com>
2016-10-16 15:24:40 -0400

    No changes really

--- src/level.cpp
+++ src/level.cpp
@@ -6,46 +6,47 @@
 Level::Level(std::string filename) {
     std::string line;
     std::ifstream infile(filename);
-    if (infile.is_open()) {
-        // get the size of the level from first line
-        infile >> this->height;
-        infile >> this->width;
+    if(!infile.is_open()){
+        std::cerr<<"Cannot open level file: "+filename<<std::endl;
+        return ;
+    }
+
+    // get the size of the level from first line
+    infile >> this->height;
+    infile >> this->width;
+    getline(infile, line);
+
+    // initialize level contents
+    levelContents = new Tiles * [this->height];
+    for (int i = 0; i < this->height; i++) {
+        levelContents[i] = new Tiles[this->width];
+    }
+
+    // read the file and build the level
+    for (int i = 0; i < this->height; i++) {
         getline(infile, line);
-
-        // initialize level contents
-        levelContents = new Tiles * [this->height];
-        for (int i = 0; i < this->height; i++) {
-            levelContents[i] = new Tiles[this->width];
-        }
-
-        // read the file and build the level
-        for (int i = 0; i < this->height; i++) {
-            getline(infile, line);
-            for (int j = 0; j < this->width; j++) {
-                switch (line.at(j)) {
-                    case '^':
-                        levelContents[i][j] = NONE;
-                        break;
-                    case '#':
-                        levelContents[i][j] = TERRAIN;
-                        break;
-                    case '@':
-                        levelContents[i][j] = SPAWN;
-                        break;
-                    case '-':
-                        levelContents[i][j] = ENEMY;
-                        break;
-                    case 'O':
-                        levelContents[i][j] = GOAL;
-                        break;
-                    default:
-                        std::cout << "Unrecognized symbol in level file " << line.at(j) << std::endl;
-                        break;
-                }
+        for (int j = 0; j < this->width; j++) {
+            switch (line.at(j)) {
+                case '^':
+                    levelContents[i][j] = NONE;
+                    break;
+                case '#':
+                    levelContents[i][j] = TERRAIN;
+                    break;
+                case '@':
+                    levelContents[i][j] = SPAWN;
+                    break;
+                case '-':
+                    levelContents[i][j] = ENEMY;
+                    break;
+                case 'O':
+                    levelContents[i][j] = GOAL;
+                    break;
+                default:
+                    std::cout << "Unrecognized symbol in level file " << line.at(j) << std::endl;
+                    break;
             }
         }
-    } else {
-        std::cout << "Could not open level file: " << filename << std::endl;
     }
 }
 
