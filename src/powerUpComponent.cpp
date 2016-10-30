38d2ce9fb4360852abb2853e7a934a30b4239589
Grady Xiao <xiao.grady@gmail.com>
2016-10-30 17:35:28 -0400

    Changed power up structures, adding infinite health power up

--- src/powerUpComponent.cpp
+++ src/powerUpComponent.cpp
@@ -3,39 +3,58 @@
 PowerUpComponent::PowerUpComponent(Entity * entity):
         Component(entity),
         timeElapsed(0),
-        infiniteJumpActivated(false)
+        infJumpActivated(false),
+        infHealthActivated(false)
 {
 }
 
 void PowerUpComponent::update(int dt) {
 
-    if(this->infiniteJumpActivated){
+    if(this->infJumpActivated || this->infHealthActivated){
         this->timeElapsed+=dt;
-
     }
 
     if(timeElapsed>=3500){ //3.5 seconds limit for all powerups
 
-        if(this->infiniteJumpActivated){
+        if(this->infJumpActivated){
             //std::cout<<"The time has expired."<<std::endl;
             this->timeElapsed=0;
-            deactivateInfiniteJumpsPowerUp();
+            deactivateInfJumpPwrUp();
+        }
+
+        if(this->infHealthActivated){
+            this->timeElapsed=0;
+            deactivateInfHealthPwrUp();
         }
 
     }
 }
 
-void PowerUpComponent::activateInfiniteJumpsPowerUp() {
+void PowerUpComponent::activateInfJumpPwrUp() {
     if(!entity->physics->isInfiniteJumpOn()){
         entity->physics->toggleInfiniteJumps();
-        this->infiniteJumpActivated=true;
+        this->infJumpActivated=true;
         //std::cout<<"The jump is activated."<<std::endl;
     }
 }
 
-void PowerUpComponent::deactivateInfiniteJumpsPowerUp() {
+void PowerUpComponent::deactivateInfJumpPwrUp() {
     if(entity->physics->isInfiniteJumpOn()){
         entity->physics->toggleInfiniteJumps();
-        this->infiniteJumpActivated=false;
+        this->infJumpActivated=false;
+    }
+}
+
+void PowerUpComponent::activateInfHealthPwrUp() {
+    if(!entity->health->isIsInvincible()){
+        entity->health->toggleInvincibility();
+        this->infHealthActivated=true;
+    }
+}
+
+void PowerUpComponent::deactivateInfHealthPwrUp() {
+    if(entity->health->isIsInvincible()){
+        entity->health->toggleInvincibility();
+        this->infHealthActivated=false;
     }
 }
