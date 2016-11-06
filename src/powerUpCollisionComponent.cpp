11fa49a363541ebaac58eb5dae2f8fc5978f129f
Andrew Zhu <azhu8@jhu.edu>
2016-11-05 20:26:20 -0400

    renamed powerup file, updated README

--- src/powerUpCollisionComponent.cpp
+++ src/powerUpCollisionComponent.cpp
@@ -2,18 +2,15 @@
 
 PowerUpCollisionComponent::PowerUpCollisionComponent(Entity *entity, Command *deletePwrUpEntityCmd,
                                                      PowerUpType pwrUpType):
-        StaticCollisionComponent(entity),
-        deletePowerUpCmd(deletePwrUpEntityCmd),
-        isClaimed(false),
-        pwrUpType(pwrUpType)
-{
-
+    StaticCollisionComponent(entity),
+    deletePowerUpCmd(deletePwrUpEntityCmd),
+    isClaimed(false),
+    pwrUpType(pwrUpType) {
 }
 
 void PowerUpCollisionComponent::onEntityCollision(DynamicCollisionComponent *otherComp) {
     if(isClaimed)
         return;
-
 
     if(otherComp->entity->powerUp){
         otherComp->entity->powerUp->activatePowerUp(this->pwrUpType);
