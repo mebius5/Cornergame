f6764f04e1aa5abdd5934e94c365cc9e84e6d11f
Akshay Srivatsan <akshay.srivatsan@gmail.com>
2016-11-06 15:36:57 -0500

    Increased spawn delay up to half a second

--- src/heroInputComponent.cpp
+++ src/heroInputComponent.cpp
@@ -10,7 +10,7 @@ HeroInputComponent::HeroInputComponent(Entity* entity, bool wasd, SpawnProjComma
     jumpPressed(false),
     spawnCommand(spawnCommand),
     spawnTime(0),
-    spawnCooldown(10),
+    spawnCooldown(500),
     holdTime(-1.0f),
     maxHold(500.0f) {
 }
