0da9b06b55c6ca198b85410eb858fb72a0355e97
Pratyush Trivedi <pratyushtrivedi@Pratyushs-MacBook-Pro.local>
2016-11-13 21:33:38 -0500

    new backgrounds

---
+++ src/backgroundArtComponent.cpp
@@ -0,0 +1,15 @@
+#include "artComponent.h"
+
+BackgroundArtComponent::BackgroundArtComponent(Entity* ent, SDL_Texture* tex, int lay, float speed) :
+    ArtComponent(ent, lay, false),
+    texture(tex),
+    speed(speed) {
+}
+
+SDL_Texture* BackgroundArtComponent::getNextTexture(int /*dt*/) {
+    return this->texture;
+}
+
+SDL_Rect* BackgroundArtComponent::getNextSrcRect(int /*dt*/) {
+    return NULL;
+}
